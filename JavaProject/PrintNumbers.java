aaf03cab5e940ef31d4d560e1c07529de886cbe7
Mikael <m.persson@accenture.com>
2017-11-22 14:54:50 +0100

    First Commit  of PrintNumbers project
    First commit

---
+++ JavaProject/PrintNumbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class PrintNumbers {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+for(int i = 100; i < 200; i++) {    
+     System.out.print(i+ ((i%10==9) ? "\n" : " "));
+}*/
